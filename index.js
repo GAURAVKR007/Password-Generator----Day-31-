9caffa51bd69a130bc05f6ff7b063460efc4ebf3
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-24 10:00:08 +0530

    Added functionality to Generate Password and show it according to user

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('Uppercase')
+const lowercaseEl = document.getElementById('Lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const genrateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -5,7 +14,37 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+genrateEl.addEventListener('click',()=>{
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
 
+    resultEl.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol ,length);
+})
+
+
+function generatePassword(lower,upper,number,symbol,length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower},{upper},{number},{symbol}].filter(item => Object.values(item)[0])
+
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i<length;i+=typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0,length)
+
+    return finalPassword
+}
 
 
 
